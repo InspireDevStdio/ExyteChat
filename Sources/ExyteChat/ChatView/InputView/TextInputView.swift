ae8ced87555fdec132bdb53a56f92f5512002f82
FireLord <firelord.xda@gmail.com>
2025-08-22 17:03:05 +0530

    Make UIkit textfield

--- Sources/ExyteChat/ChatView/InputView/TextInputView.swift
+++ Sources/ExyteChat/ChatView/InputView/TextInputView.swift
@@ -16,21 +16,21 @@ struct TextInputView: View {
     var availableInputs: [AvailableInputType]
     var localization: ChatLocalization
     
-    var body: some View {
-        TextField("", text: $text, prompt: Text(localization.inputPlaceholder)
-            .foregroundColor(theme.colors.inputPlaceholderText), axis: .vertical)
-            .customFocus($globalFocusState.focus, equals: .uuid(inputFieldId))
-            .foregroundColor(theme.colors.inputText)
-            .padding(.vertical, 10)
-            .padding(.leading, !isMediaGiphyAvailable() ? 12 : 0)
-            .onTapGesture {
-                globalFocusState.focus = .uuid(inputFieldId)
-            }
-    }
+    @State private var textViewHeight: CGFloat = 48
     
-    private func isMediaGiphyAvailable() -> Bool {
-        return availableInputs.contains(AvailableInputType.media)
-        || availableInputs.contains(AvailableInputType.giphy)
+    var body: some View {
+        CustomTextField(
+            text: $text,
+            inputFieldId: inputFieldId,
+            style: style,
+            availableInputs: availableInputs,
+            localization: localization,
+            textViewHeight: $textViewHeight
+        )
+        .frame(height: textViewHeight)
+        .onTapGesture {
+            globalFocusState.focus = .uuid(inputFieldId)
+        }
     }
 }
 
